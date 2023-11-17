9202a49fa613e7ed4022e82363b2ebe3c10e4ad6
VishnuAravind12 <vishnuaa77@gmail.com>
2023-11-17 09:02:14 -0800

    modified sorter to add mult run timing

--- src/main/java/com/nighthawk/spring_portfolio/sorters/Sorter.java
+++ src/main/java/com/nighthawk/spring_portfolio/sorters/Sorter.java
@@ -1,24 +1,39 @@
 package com.nighthawk.spring_portfolio.sorters;
 
+import java.util.Random;
+
 public abstract class Sorter {
 
-    /**
-     * Sorts the given array.
-     * 
-     * @param array The array to be sorted.
-     */
+    private static final Random random = new Random();
+
     public abstract void sort(int[] array);
 
-    /**
-     * Measures the time taken to sort an array.
-     * 
-     * @param array The array to be sorted.
-     * @return The time taken to sort the array in milliseconds.
-     */
     public long timeSorting(int[] array) {
         long startTime = System.currentTimeMillis();
         sort(array);
         long endTime = System.currentTimeMillis();
         return endTime - startTime;
+    }
+
+    public void benchmarkSort(int minSize, int maxSize, int stepSize, int runs) {
+        for (int size = minSize; size <= maxSize; size += stepSize) {
+            long totalTime = 0;
+
+            for (int run = 0; run < runs; run++) {
+                int[] data = generateRandomData(size);
+                totalTime += timeSorting(data);
+            }
+
+            double averageTime = totalTime / (double) runs;
+            System.out.println("Array Size: " + size + ", Average Time: " + averageTime + " ms");
+        }
+    }
+
+    private int[] generateRandomData(int size) {
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = random.nextInt(1000); // Random integer between 0 and 999
+        }
+        return data;
     }
 }

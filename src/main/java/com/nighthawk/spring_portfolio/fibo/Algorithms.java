6598e23749763618baae0ee1f4325bd957754ac0
vardaansinha <vardsin28@gmail.com>
2023-11-29 21:13:19 -0800

    importing necessary modules, including SWING

--- src/main/java/com/nighthawk/spring_portfolio/fibo/Algorithms.java
+++ src/main/java/com/nighthawk/spring_portfolio/fibo/Algorithms.java
@@ -1,5 +1,16 @@
 package com.nighthawk.spring_portfolio.fibo;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.*;
+import java.awt.*;
+
+
 public class Algorithms {
     
 }

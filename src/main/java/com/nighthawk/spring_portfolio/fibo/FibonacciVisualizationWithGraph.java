d1340471bde56c1418fab314612295e3edd00625
vardaansinha <vardsin28@gmail.com>
2023-11-30 21:47:22 -0800

    fixed file

--- src/main/java/com/nighthawk/spring_portfolio/fibo/FibonacciVisualizationWithGraph.java
+++ src/main/java/com/nighthawk/spring_portfolio/fibo/FibonacciVisualizationWithGraph.java
@@ -10,32 +10,40 @@ import org.jfree.data.xy.XYSeriesCollection;
 import javax.swing.*;
 import java.awt.*;
 
+import java.util.Scanner;
+
+
 public class FibonacciVisualizationWithGraph {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // main JFrame
+            // Step 1: Create the main JFrame
             JFrame frame = new JFrame("Fibonacci Algorithm Efficiency Visualization");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new BorderLayout());
 
-            // JFreeChart chart panel
+            // Step 2: Create the chart panel using JFreeChart
             JPanel chartPanel = createChartPanel();
             frame.add(chartPanel, BorderLayout.CENTER);
 
-            // JFrame properties
+            // Step 3: Set up the JFrame properties
             frame.setSize(800, 600);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
     }
 
+    /**
+     * Create a JPanel with a line chart to visualize the efficiency of Fibonacci algorithms.
+     *
+     * @return ChartPanel containing the line chart.
+     */
     private static JPanel createChartPanel() {
-        // getting the mock data using MockDataGenerator file from Vishnu
+        // Step 1: Generate mock data using MockDataGenerator
         int numTerms = getUserInput();
         MockData mockData = generateFibonacciMockData(numTerms);
 
-        // fibo algorithm classes
+        // Step 2: Concrete Classes for Fibonacci Algorithms
         FibonacciAlgorithm recursiveFibonacci = new RecursiveFibonacci();
         FibonacciAlgorithm iterativeFibonacci = new IterativeFibonacci();
         FibonacciAlgorithm dynamicProgrammingFibonacci = new DynamicProgrammingFibonacci();
@@ -45,13 +53,13 @@ public class FibonacciVisualizationWithGraph {
         XYSeries seriesIterative = visualizeAlgorithmEfficiency(iterativeFibonacci, mockData);
         XYSeries seriesDynamicProgramming = visualizeAlgorithmEfficiency(dynamicProgrammingFibonacci, mockData);
 
-        // making the actual dataset
+        // Step 4: Create dataset
         XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(seriesRecursive);
         dataset.addSeries(seriesIterative);
         dataset.addSeries(seriesDynamicProgramming);
 
-        // chart labels
+        // Step 5: Create chart
         JFreeChart chart = ChartFactory.createXYLineChart(
                 "Fibonacci Algorithm Efficiency",
                 "Number of Terms",
@@ -63,11 +71,136 @@ public class FibonacciVisualizationWithGraph {
                 false
         );
 
-        // creating the actual panel
+        // Step 6: Create panel
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new Dimension(800, 600));
 
-        // returning the output
+        // Step 7: Return the chart panel
         return chartPanel;
     }
 
+    private static int getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms in the Fibonacci sequence: ");
+        return scanner.nextInt();
+    }
+
+
+    private static MockData generateFibonacciMockData(int numTerms) {
+        double[][] xData = new double[numTerms][4];
+        double[] yData = new double[numTerms];
+
+        for (int i = 0; i < numTerms; i++) {
+            // Use Fibonacci sequence for xData
+            xData[i][0] = fibonacci(i + 1);  // Commits
+            xData[i][1] = fibonacci(i + 2);  // Pull Requests
+            xData[i][2] = fibonacci(i + 3);  // Issues
+            xData[i][3] = fibonacci(i + 4);  // Repos Contributed
+
+            // Generate yData based on the Fibonacci terms
+            yData[i] = fibonacci(i + 1) + fibonacci(i + 2) + fibonacci(i + 3) + fibonacci(i + 4);
+        }
+
+        return new MockData(xData, yData);
+    }
+
+
+    private static int fibonacci(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    private static XYSeries visualizeAlgorithmEfficiency(FibonacciAlgorithm algorithm, MockData mockData) {
+        XYSeries series = new XYSeries(algorithm.getClass().getSimpleName());
+
+        for (int i = 1; i <= mockData.getNumTerms(); i++) {
+            long startTime = System.nanoTime();
+            algorithm.calculateFibonacci(i);
+            long endTime = System.nanoTime();
+
+            long elapsedTime = endTime - startTime;
+            series.add(i, elapsedTime);
+        }
+
+        return series;
+    }
+
+    
+    abstract static class FibonacciAlgorithm {
+        abstract long calculateFibonacci(int n);
+    }
+
+   
+    static class RecursiveFibonacci extends FibonacciAlgorithm {
+        @Override
+        long calculateFibonacci(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+        }
+    }
+
+    
+    static class IterativeFibonacci extends FibonacciAlgorithm {
+        @Override
+        long calculateFibonacci(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            long a = 0, b = 1;
+            for (int i = 2; i <= n; i++) {
+                long temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return b;
+        }
+    }
+
+    /**
+     * Concrete class representing a dynamic programming Fibonacci algorithm.
+     */
+    static class DynamicProgrammingFibonacci extends FibonacciAlgorithm {
+        @Override
+        long calculateFibonacci(int n) {
+            if (n <= 1) {
+                return n;
+            }
+            long[] dp = new long[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+
+    /**
+     * Class to encapsulate mock data.
+     */
+    private static class MockData {
+        private final double[][] xData;
+        private final double[] yData;
+
+        public MockData(double[][] xData, double[] yData) {
+            this.xData = xData;
+            this.yData = yData;
+        }
+
+        public double[][] getXData() {
+            return xData;
+        }
+
+        public double[] getYData() {
+            return yData;
+        }
+
+        public int getNumTerms() {
+            return xData.length;
+        }
+    }
+}

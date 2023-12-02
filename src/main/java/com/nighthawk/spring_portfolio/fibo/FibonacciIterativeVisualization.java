ed6efa0f815179b4aa55bfa5d0874097c1e9d001
vardaansinha <vardsin28@gmail.com>
2023-12-02 12:41:11 -0800

    same bulk of file

--- src/main/java/com/nighthawk/spring_portfolio/fibo/FibonacciIterativeVisualization.java
+++ src/main/java/com/nighthawk/spring_portfolio/fibo/FibonacciIterativeVisualization.java
@@ -1,5 +1,33 @@
 package com.nighthawk.spring_portfolio.fibo;
 
 public class FibonacciIterativeVisualization {
-    
-}
+
+    public static void main(String[] args) {
+        // Get user input for the number of terms
+        int n = getUserInput();
+
+        // Generate Fibonacci terms using iterative algorithm and measure time
+        double[] xData = new double[n];
+        double[] yData = new double[n];
+
+        for (int i = 1; i <= n; i++) {
+            long startTime = System.nanoTime();
+            int fibonacciTerm = fibonacciIterative(i);
+            long endTime = System.nanoTime();
+            long timeTaken = endTime - startTime;
+
+            // Store data for visualization
+            xData[i - 1] = i;
+            yData[i - 1] = timeTaken;
+        }
+
+        // Create and display the chart
+        FiboVisualizationUtil.displayChart(xData, yData, "Fibonacci Iterative");
+    }
+
+    private static int getUserInput() {
+        // testing fixed value of 10
+        return 10;
+    }
+
+   

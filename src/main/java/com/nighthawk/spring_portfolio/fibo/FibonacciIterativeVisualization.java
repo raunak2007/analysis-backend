65d8578b2db4748c06276cfaafb0e6e1d77ce07a
vardaansinha <vardsin28@gmail.com>
2023-12-02 12:44:25 -0800

    adding genuine iterative algo

--- src/main/java/com/nighthawk/spring_portfolio/fibo/FibonacciIterativeVisualization.java
+++ src/main/java/com/nighthawk/spring_portfolio/fibo/FibonacciIterativeVisualization.java
@@ -30,4 +30,20 @@ public class FibonacciIterativeVisualization {
         return 10;
     }
 
-   
+    private static int fibonacciIterative(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int fib = 1;
+        int prev = 1;
+
+        for (int i = 2; i < n; i++) {
+            int temp = fib;
+            fib += prev;
+            prev = temp;
+        }
+
+        return fib;
+    }
+}

b2979d5c7c54ca9ea5e6be932cc8871c902c9e30
Raunak Mondal <spmondal08@gmail.com>
2023-12-10 22:26:21 -0800

    Update FiboJpaRepository.java

--- src/main/java/com/nighthawk/spring_portfolio/fibo/FiboJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/fibo/FiboJpaRepository.java
@@ -1,15 +1,14 @@
 package com.nighthawk.spring_portfolio.fibo;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
+import java.util.List;
+
 public interface FiboJpaRepository extends JpaRepository<Jokes, Long> {
-    /* JPA has many built in methods: https://www.tutorialspoint.com/spring_boot_jpa/spring_boot_jpa_repository_methods.htm
-    The below custom methods are prototyped for this application
-    */
+
     void save(String Joke);
+
     List<Jokes> findAllByOrderByJokeAsc();
+
     List<Jokes> findByJokeIgnoreCase(String joke);
 }

6be7bd58e01a5dec2235a8cdbfa4cb1c1da95313
Raunak Mondal <spmondal08@gmail.com>
2023-12-10 22:59:40 -0800

    Update FiboJpaRepository.java

--- src/main/java/com/nighthawk/spring_portfolio/fibo/FiboJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/fibo/FiboJpaRepository.java
@@ -2,13 +2,6 @@ package com.nighthawk.spring_portfolio.fibo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface FiboJpaRepository extends JpaRepository<Jokes, Long> {
-
-    void save(String Joke);
-
-    List<Jokes> findAllByOrderByJokeAsc();
-
-    List<Jokes> findByJokeIgnoreCase(String joke);
+public interface FiboJpaRepository extends JpaRepository<Object, Long> {
+    
 }
